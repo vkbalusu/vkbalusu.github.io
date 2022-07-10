ae06413b4d78ff11598997b25745f4519b94a673
Vamsi Krishna Balusu <vkbalusu@gmail.com>
2022-07-09 18:05:23 -0700

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.0264b49ff0260cd98098045f10a642e2.js"
+  "/precache-manifest.0ecf37f7079754b14142fe8a3fad6f50.js"
 );
 
 self.addEventListener('message', (event) => {
